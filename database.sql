ac515c9a9aba8acdffe8648f3a533afeac17391d
Tony Edwardson <tony@edwardson.co.uk>
2014-04-27 19:35:54 +0200

    various

--- database.sql
+++ database.sql
@@ -3,11 +3,14 @@ create table if not exists user (id INTEGER PRIMARY KEY ASC not null,
                                  pass TEXT not null);
 
 create table if not exists project (id INTEGER PRIMARY KEY ASC not null,
-                                    name TEXT not null);
+                                    name TEXT not null,
+                                    description TEXT not null
+                                    );
  
 
 create table if not exists repository (id INTEGER PRIMARY KEY ASC not null,
                                         name TEXT not null,
+                                        description TEXT not null,
                                         is_public INTEGER not null,
                                         projectid INTEGER not null,
                                         FOREIGN KEY(projectid) REFERENCES project(id) );
--- lib/GitWeb.pm
+++ lib/GitWeb.pm
@@ -2,9 +2,11 @@ package GitWeb;
 use Dancer2;
 use Crypt::PBKDF2;
 use Dancer2::Plugin::Database;
+use Data::Dumper;
 
 our $VERSION = '0.1';
 my $dbh = database('gitweb.sqlite');
+my $salt = 'somethingfisshy';
 my $pbkdf2 = Crypt::PBKDF2->new(
 	hash_class => 'HMACSHA2',
 	hash_args => {
@@ -21,6 +23,18 @@ get '/' => sub {
 get '/login' => sub {
     template 'login', { path => vars->{requested_path} };
 };
+post '/login' => sub {
+    chomp params->{pass};
+    my $hash = $pbkdf2->generate( params->{pass}, $salt );
+    my $rows = database->quick_count('user', { user => params->{user}, pass => $hash });
+    unless ($rows == 1) {
+        redirect '/login?failed=1';
+    }
+    else {
+        session user => params->{user};
+        redirect params->{requested_path} || '/';
+    }
+};
 get '/register' => sub {
     template 'register';
 };
@@ -33,28 +47,57 @@ post '/register' => sub {
         redirect '/register';
     }
     database->quick_insert('user', { user => params->{user} ,
-                                     pass => $pbkdf2->generate( params->{pass} ) });
+                                     pass => $pbkdf2->generate( params->{pass}, $salt ) });
 
-
-
-    template 'register';
+    session user => params->{user};
+    redirect '/';
 };
 
-post '/login' => sub {
-    my $rows = database->quick_count('user', { user => params->{user}, pass => $pbkdf2->generate( params->{pass} ) });
-    unless ($rows == 1) {
-        redirect '/login?failed=1';
-    }
-    else {
-        session user => params->{user};
-        redirect params->{requested_path} || '/';
-    }
-};
 any '/logout' => sub {
-
     session->delete(session('user'));
     template 'login';
     redirect '/login';
+};
+
+get '/newproject' => sub {
+     template 'newproject', { error => params->{error} };
+};
+post '/newproject' => sub {
+    unless (params->{name}) {
+        redirect "/newproject?error=Mandatory param Name not supplied";
+    }
+    unless (params->{desc}) {
+        redirect "/newproject?error=Mandatory param Description not supplied";
+    }
+    my $rows = database->quick_count('project', { name => params->{name} });
+    unless ($rows == 0) {
+        redirect '/newproject?error=Project already exists';
+    }
+    database->quick_insert('project', { name => params->{'name'},
+                                     description => params->{'desc'} } );
+    redirect '/project/';
+
+};
+get '/newrepo' => sub {
+     template 'newrepo', { project => params->{project}, error => params->{error}  };
+};
+post '/newrepo' => sub {
+    unless (params->{project}) {
+        redirect "/newrepo?error=Mandatory param Project not supplied";
+    }
+    unless (params->{name}) {
+        redirect '/newrepo?error="Mandatory param Name not supplied"';
+    }
+    unless (params->{desc}) {
+        redirect '/newrepo?error="Mandatory param Description not supplied"';
+    }
+    my $rows = database->quick_count('repository', { name => params->{name} });
+    unless ($rows == 0) {
+        redirect '/newproject?error="Repository already exists"';
+    }
+    database->quick_insert('repo', { name => params->{'name'},
+                                     description => params->{'desc'} } );
+    redirect '/repo/'
 };
 sub check_logged_in {
   if (! session('user') && request->path_info !~ m{^/login}) {
