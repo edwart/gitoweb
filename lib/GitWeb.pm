3402b5989ff5f31d3a95cdd9a43be7ec21b34724
Tony Edwardson <tony.edwardsson@seb.se>
2015-03-12 12:18:38 +0100

    In testing

--- lib/GitWeb.pm
+++ lib/GitWeb.pm
@@ -1,11 +1,13 @@
 package GitWeb;
 use FindBin qw/  $Bin /;
-use File::Basename qw/ dirname /;
+use File::Basename qw/ dirname basename /;
+use File::Find::Rule;
+use IPC::System::Simple qw/system systemx capture capturex run runx $EXITVAL EXIT_ANY/;
 use feature 'say';
 use File::HomeDir;
 use Dancer2;
 use Crypt::PBKDF2;
-use Dancer2::Plugin::Database;
+use Try::Tiny;
 use Git;
 use Template;
 use Sys::Hostname;
@@ -28,27 +30,39 @@ my $gitpath = config->{'gitpath'};
 my $repodir = config->{'repodir'};
 my $home = File::HomeDir->my_home;
 my $gitolite_admin = "$home/gitolite-admin";
-my $gitolite_conf = "$home/gitolite-admin/conf/gitolite.conf";
+my $gitolite_conf = "${gitolite_admin}/conf/gitolite.conf";
+my $gitoweb_cfg = dirname($Bin)."/gitoweb.cfg";
+our $config;
+unless (-f $gitoweb_cfg) {
+    $config = { users => {},
+               repos => {},
+               projects => {},
+               groups => { all => {} },
+               sshkeys => {},
+           };
+    save_config();
+}
+load_config();
 set session_dir => dirname($Bin).'/sessions';
 
 
-=for 
 hook before => sub {
 	unless (-d $gitolite_admin ) {
+        debug "\$gitolite_admin $gitolite_admin doesn't exist";
 		initial_setup();
 	}
-	if (!session('user') && request->dispatch_path !~ m{^/(login|register)}) {
-		# Pass the original path requested along to the handler:
-		forward '/login', { requested_path => request->dispatch_path };
-	}
+    get_gitolite_config();
+#	if (!session('user') && request->dispatch_path !~ m{^/(login|register)}) {
+#		# Pass the original path requested along to the handler:
+#		forward '/login', { requested_path => request->dispatch_path };
+#	}
 };
-=cut
 
 get '/' => sub {
 #	reconfig_gitolite();
-    my @repos = database->quick_select('repos', {});
-    template 'index', { repos => \@repos };
+    template 'index';
 };
+
 
 get '/reset' => sub {
 	reset_it();
@@ -57,21 +71,18 @@ get '/reset' => sub {
 
 get '/setup' => sub {
 	initial_setup();
+    get_gitolite_config();
 	forward '/';
 };
 get '/users' => sub {
-    my @users = database->quick_select('user', {});
-    template 'useradmin', { users => \@users };
+    template 'useradmin', { users => $config->{users} };
 };
 get '/login' => sub {
     template 'login', { path => vars->{requested_path} };
 };
 post '/login' => sub {
     chomp params->{pass};
-    my $hash = $pbkdf2->generate( params->{pass}, $salt );
-    my $rows = database->quick_count('user', { name => params->{user}, pass => $hash });
-	debug Dump(rows => $rows);
-    unless ($rows == 1) {
+    unless (Exists($config->{users}, params->{user}) and $config->{users}{ params->{user} }{Password} eq params->{pass}) {
         redirect '/login?failed=1';
     }
     else {
@@ -90,12 +101,14 @@ post '/register' => sub {
 		debug "No user or pass supplied";
         redirect '/register';
     }
-    my $rows = database->quick_count('user', { name => params->{user} });
-	debug Dump(rows => $rows);
+    if (Exists($config->{users}, params->{user})) {
+        redirect '/register?failed=1';
+    }
 	debug "insert ".params->{user};
-    database->quick_insert('user', { name => params->{user} ,
-                                     pass => $pbkdf2->generate( params->{pass}, $salt ) });
-
+    $config->{users}{ params->{user} }{Password} = crypt params->{pass}, __PACKAGE__;
+    save_config();
+    my $output = Data::Dumper->Dump([params->{user}, $config], [qw/$user $config/]);
+    debug $output;
     session user => params->{user};
     redirect '/';
 };
@@ -106,15 +119,11 @@ any '/logout' => sub {
     redirect '/login';
 };
 get '/project/:project' => sub {
-    my $project = database->quick_select('project', {name => params->{project}});
-    my @repos = database->quick_select('repository', {name => params->{project}});
-	template 'project', { repos => \@repos, project => $project };
+	template 'project', { repos => $config->{repos}, project => params->{project} };
 };
 
 get '/projects' => sub {
-    my @projects = database->quick_select('project', {});
-	debug Dump(projects => \@projects);
-	template 'projects', { projects => \@projects, error => params->{error} };
+	template 'projects', { projects => $config->{projects}, error => params->{error} };
 };
 get '/newproject' => sub {
      template 'newproject', { error => params->{error} };
@@ -126,24 +135,25 @@ post '/newproject' => sub {
     unless (params->{desc}) {
         redirect "/newproject?error=Mandatory param Description not supplied";
     }
-    my $rows = database->quick_count('project', { name => params->{name} });
-    unless ($rows == 0) {
+    if (Exists($config->{projects}, params->{name})) {
         redirect '/newproject?error=Project already exists';
     }
     debug "making project in ".params->{name};
-    database->quick_insert('project', { name => params->{'name'},
-                                     description => params->{'desc'} } );
+    $config->{projects}{params->{name}} = { name => params->{name},
+                                            desc => params->{'desc'},
+                                            repos => {},
+                                        };
+    save_config();
     redirect '/';
 
 };
 get '/newrepo' => sub {
-    my @projects = database->quick_select('project', {});
-    template 'newrepo', {  projects => \@projects, error => params->{'error'}  };
+    my $output = Data::Dumper->Dump([$config], [qw/$config/]);
+    debug "before newrepo $output";
+    template 'newrepo', {  projects => $config->{projects}, error => params->{'error'}  };
 };
 get '/newrepo/:project' => sub {
-    my @projects = database->quick_select('project', {});
-    my $project = database->quick_select('project', {name => params->{project}});
-    template 'newrepo', {  projects => \@projects, currentproject => params->{project}, error => params->{'error'}  };
+    template 'newrepo', {  projects => $config->{projects}, currentproject => params->{project}, error => params->{'error'}  };
 };
 post '/newrepo' => sub {
 	my %params = params;
@@ -154,18 +164,25 @@ post '/newrepo' => sub {
     unless (params->{desc}) {
         redirect '/newrepo?error="Mandatory param Description not supplied"';
     }
-    my $rows = database->quick_count('repository', { name => params->{'name'} });
-    unless ($rows == 0) {
+    if (Exists($config->{projects}, params->{'name'})) {
         redirect '/newproject?error="Repository already exists"';
     }
-	my $project = database->quick_select('project', {name => params->{project}});
-	debug Dump(project=> $project);
+    my $output = Data::Dumper->Dump([$config, ], [qw/$config/]);
+    debug "before newrepo $output";
+    $config->{repos}{ params->{'name'} } = {
+                                                'RW+' => { '@all' => '@all' },
+                                                name => params->{'name'},
+                                                project => params->{'project'},
+                                                description => params->{'desc'},
+                                                is_public => params->{public},
+                                               };
+    $config->{projects}{ params->{'project'} }{repos}{ params->{'name'} } = { 
+                                                                 name => params->{'name'},
+                                                                 description => params->{'desc'},
+                                                                 is_public => params->{public}
+                                                             };
 
-    database->quick_insert('repository', { name => params->{'name'},
-                                     description => params->{'desc'},
-									 projectid => $project->{id},
-							 		 is_public => params->{public} eq 'on'});
-
+    save_config();
 	reconfig_gitolite();
 	redirect '/';
 
@@ -180,22 +197,24 @@ get '/repo/*/*' => sub {
 	template 'repo', { user => $login, hostname => $hostname, repo => $repo , project => $project, files => \@files};
 };
 get '/sshkeys' => sub {
-	my @sshkeys = database->quick_select('sshkeys', {});
-	template 'sshkeys', { sshkeys => \@sshkeys };
+	template 'sshkeys', { sshkeys => $config->{sshkeys} };
 };
 get '/addsshkey' => sub {
 	template 'addsshkey';
 };
+get '/save' => sub {
+    load_config();
+    reconfig_gitolite();
+	redirect '/';
+};
 post '/addsshkey' => sub {
 	my %params = params;
 	debug Dump(params=> \%params);
-	my $count = database->quick_count('sshkeys', { name => params->{'keyname'} });
-	debug Dump(count => $count);
-	if ($count < 1) {
-		database->quick_insert('sshkeys', { name => params->{'keyname'},
-											sshkey => params->{'sshkey'},
-										  }
-							  );
+    if (Exists($config->{sshkeys}, params->{'keyname'})) {
+           redirect '/addsshkey?error=key already exists';
+    }
+    else {
+        $config->{sshkeys}{ params->{'keyname'} } = params->{'sshkey'};
 		my $gitolite_keydir = "$home/gitolite-admin/keydir";
 		my $keyfile = join("/",$gitolite_keydir, params->{'keyname'}.".pub");
 		my $fh;
@@ -204,24 +223,18 @@ post '/addsshkey' => sub {
 		$fh = new FileHandle ">$keyfile" or error "can't write $keyfile: $!";
 		$fh->print(params->{'sshkey'});
 		$fh->close;
-		system("git add keydir");
+		system("git add $keyfile");
 		system(qq!git commit -m "added $keyfile"!);
+        save_config();
 		reconfig_gitolite();
+		system(qq!git push"!);
 	}
 	redirect '/sshkeys';
 };
 sub reconfig_gitolite {
-	my @sshkeys = database->quick_select('sshkeys', {});
-	debug Dump(sshkeys => \@sshkeys);
-	my @repos = database->quick_select('repos', {});
-	my @users = database->quick_select('user', {});
-	my @usernames = map { $_->{name} } @sshkeys;
-	my @groups = database->quick_select('groups', {});
-	my @group_members = database->quick_select('group_members', {});
-
 	my @changes = ();
-	my %groups = map { $_->{name} => {} } @groups;
-	my $processed = template 'gitolite.conf', { users => \@users, usernames => \@usernames, groups => \@groups, repos => \@repos }, {layout => 0 };
+	my %groups = map { $_ => {} } keys %{ $config->{groups} };
+	my $processed = template 'gitolite.conf', { config => $config, env => \%ENV }, {layout => 0 };
 	debug "processed = ".$processed;
 	my $fh = new FileHandle "> $gitolite_conf" or die "Whoops: can't write to $gitolite_conf: $!";
 	$fh->print($processed);
@@ -235,36 +248,131 @@ sub reconfig_gitolite {
 }
 sub reset_it {
 	debug "resetting";
-	foreach my $what (qw!bin gitolite-admin repositories projects.list git2 git2.pub .gitolite* .ssh sessions!) {
+	foreach my $what (qw!bin/* gitolite-admin repositories projects.list *.pub .gitolite* .ssh/* sessions!) {
 		debug "rm -rf $home/$what";
 		my $fh = new FileHandle "rm -rf $home/$what 2>&1|";
 		while (my $line = <$fh>) {
 			debug $line;
 		}
 	}
-	chdir ("$home/gitoweb");
-	my $fh = new FileHandle "sqlite3 gitweb.sqlite < database.sql|";
-	while (my $line = <$fh>) {
-		debug $line;
-	}
-	debug "reset";
 }
 sub initial_setup {
 	unless (-d $gitolite_admin ) {
-		say(qq!ssh-keygen -b 2048 -t rsa -f $home/.ssh/id_rsa -P "" -q!);
-		system(qq!ssh-keygen -b 2048 -t rsa -f $home/.ssh/id_rsa -P "" -q!);
+		chdir $home;
+        $ENV{GIT_SSL_NO_VERIFY}="true";
+        debug "/opt/app/murex/SEB/bin/git clone https://github.com/sitaramc/gitolite.git";
+        try {
+            system("/opt/app/murex/SEB/bin/git clone https://github.com/sitaramc/gitolite.git");
+        };
+        if (-e "$home/.ssh/gitolite") {
+            debug "rm -rf $home/.ssh/gitolite_admin";
+            system("rm -rf $home/.ssh/gitolite_admin");
+        }
+		say(qq!ssh-keygen -b 2048 -t rsa -f $home/.ssh/gitolite_admin -P "" -q!);
+		system(qq!ssh-keygen -b 2048 -t rsa -f $home/.ssh/gitolite_admin -P "" -q!);
 		my $fh = new FileHandle "> $home/.ssh/config";
 		$fh->print("Host *\n\tStrictHostKeyChecking no\n");
 		$fh->close;
 	}
 	unless (-d  "$home/bin") {
 		mkdir("$home/bin");
+    }
 		say(qq!$home/gitolite/install -to $home/bin!);
 		system(qq!$home/gitolite/install -to $home/bin!);
-		say(qq!$home/bin/gitolite setup -pk  $home/.ssh/id_rsa.pub!);
-		system(qq!$home/bin/gitolite setup -pk  $home/.ssh/id_rsa.pub!);
+		say(qq!$home/bin/gitolite setup -pk  $home/.ssh/gitolite_admin.pub!);
+		system(qq!$home/bin/gitolite setup -pk  $home/.ssh/gitolite_admin.pub!);
 		chdir $home;
-		system(qq!git clone ${repodir}/gitolite-admin.git!);
-	}
+        unless (-d "${home}/gitolite-admin" ) {
+    debug qq!/opt/app/murex/SEB/bin/git clone ${repodir}/gitolite-admin.git!;
+    system(qq!/opt/app/murex/SEB/bin/git clone ${repodir}/gitolite-admin.git!);
+    }
+}
+sub get_gitolite_config {
+    my $output = Data::Dumper->Dump([$config], [qw/$config/]);
+    debug "get_gitolite_conf $output";
+    my $fh = new FileHandle "<$gitolite_conf" or die "Can't read $gitolite_conf: $!";
+    my @sshkeys = File::Find::Rule->file()->in( "$gitolite_admin/keydir" );
+    foreach my $keyfile (@sshkeys) {
+        next if $keyfile =~ m/gitolite_admin.pub/;
+        $config->{sshkeys}{ basename($keyfile) } = read_file($keyfile);
+        my $user = basename($keyfile);
+        $user =~ s/\.pub//;
+        $config->{users}{ $user } = basename($keyfile);
+    }
+    while (my $line = <$fh>) {
+        chomp $line;
+        $line =~ s/#.*$//;
+        if ($line =~ m/^\@(\w+)\s*=\s*(.*)$/) {
+            my $group = $1;
+            my @users = split(/\s+/, $2);
+            $config->{groups}{ $group } = [ @users ];
+        }
+        if ($line =~ m/^repo/) {
+            next if $line =~ m/gitolite-admin/;
+            debug "Processing repo $line";
+            my @repos = split(/\s+/, $line);
+            shift @repos;
+            my $rp =  Data::Dumper->Dump([\@repos], [qw/$repos/]);
+            debug "repos $rp";
+            $line = <$fh>;
+            $line =~ s/#.*$//;
+            while($line !~ m/^\s*$/) {
+                my ($perms, $things) = split(/\s+=\s+/, $line);
+                debug "\$perms = $perms, \$things = $things";
+                $perms =~ s/^\s+//;
+                my @things = split(/\s+/, $things);
+                foreach my $repo (@repos) {
+                    if ($repo =~ m!/!) {
+                        my @bits = split("/", $repo);
+                        $config->{projects}{$bits[0]}{$repo}=$bits[1];
+                    }
+                    foreach my $thing (@things) {
+                        $config->{repos}{ $repo } {$perms}{$thing} = $thing;
+                    }
+                }
+                $line = <$fh>;
+                last unless defined $line;
+                $line =~ s/#.*$//;
+            }
+        }
+    }
+    save_config();
+    $output = Data::Dumper->Dump([$config], [qw/$config/]);
+    debug "after get_gitolite_config $output";
+}
+sub parse_config {
+}
+sub read_file {
+    my ($filename) = @_;
+    debug "Reading $filename";
+    my $fh = new FileHandle "<$filename" or die "Can't read $filename: $!";
+    my @lines = <$fh>;
+    chomp(@lines);
+    $fh->close;
+    my $contents =  join("\n", @lines);
+    debug "contents $contents";
+    return $contents;
+}
+sub Exists {
+    my ($hash, $key) = @_;
+    return undef unless defined $hash;
+    my %copy = %{ $hash };
+    return exists $copy{ $key };
+}
+sub save_config {
+    debug "saving to $gitoweb_cfg";
+    my $output = Data::Dumper->Dump([$config], [qw/$config/]);
+    debug "$output";
+    my $fh = new FileHandle ">$gitoweb_cfg";
+    $fh->print( $output );
+    $fh->close;
+}
+sub load_config {
+    my $output = Data::Dumper->Dump([$config, $gitoweb_cfg ], [qw/$config $gitoweb_cfg/]);
+    debug "before load $output";
+
+    do $gitoweb_cfg;
+    $output = Data::Dumper->Dump([$config], [qw/$config/]);
+    debug "after load $output";
 }
 true;
